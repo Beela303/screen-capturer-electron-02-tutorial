c5861840261593a9c6efef827bbee7a95b4acd64
Beela303 <devbeela@gmail.com>
2025-12-10 18:50:29 +0100

    ui, renderer, main process

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const {app, BrowserWindow} = require("electron")
 
 //Is app ready and initialized ? show the app window
 app.whenReady().then(() => {
-    const window = new BrowserWindow()
+    const window = new BrowserWindow({
+        frame: false,
+        transparent: true,
+    })
     window.loadFile("index.html")
 })
